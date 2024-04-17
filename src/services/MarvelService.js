0e0aa5ffe92e882d17a7f6da9d6a940637636df1
Nataliia Zhydychyn <nataliia.zhydychyn@gmail.com>
2024-04-17 18:14:22 +0200

    add descr to random char

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,37 +3,35 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=31ca59d60bf5f3e6f17b1ce722e04d4f';
-    getRecourse = async (url) => {
+    getResource = async (url) => {
         let res = await fetch(url);
-
-        if(!res.ok) {
-            throw new Error(`Could not fetch ${url}, status:${res.status}`);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-
+    
         return await res.json();
     }
 
     getAllCharacters = async () => {
-        const res = await this.getRecourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
     getCharacter = async (id) => {
-        const res = await this.getRecourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
         return this._transformCharacter(res.data.results[0]);
     }
 
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url,
+            wiki: char.urls[1].url
         }
     }
-
-
 }
 
 export default MarvelService;
